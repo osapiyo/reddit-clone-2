eb39fc93e850a266edbae8ba459b5442653ff2c3
osapiyo <osapiyo1@gmail.com>
2022-05-29 19:28:54 +0900

    added authmodal

---
+++ src/components/Modal/Auth/SignUp.tsx
@@ -0,0 +1,121 @@
+import { Input, Button, Flex, Text } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { useSetRecoilState } from 'recoil'
+import { authModalState } from '../../../atoms/authModalAtom'
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth'
+import { auth } from '../../../firebase/clientApp'
+import { create } from 'domain'
+
+const SignUp: React.FC = () => {
+  const setAuthModalState = useSetRecoilState(authModalState)
+  const [signUpForm, setSignUpForm] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  })
+
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth)
+
+  const onSubmit = () => {
+    createUserWithEmailAndPassword(signUpForm.email, signUpForm.password)
+  }
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSignUpForm((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }))
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <Input
+        required
+        name='email'
+        placeholder='email'
+        type='email'
+        mb={2}
+        onChange={onChange}
+        fontSize='10px'
+        _placeholder={{ color: 'gray.500' }}
+        _hover={{
+          bg: 'white',
+          border: '1px solid',
+          borderColor: 'blue.500',
+        }}
+        _focus={{
+          outline: 'none',
+          bg: 'white',
+          border: '1px solid',
+          borderColor: 'blue.500',
+        }}
+        bg='gray.50'
+      />
+      <Input
+        required
+        name='password'
+        placeholder='password'
+        type='password'
+        mb={2}
+        onChange={onChange}
+        fontSize='10px'
+        _placeholder={{ color: 'gray.500' }}
+        _hover={{
+          bg: 'white',
+          border: '1px solid',
+          borderColor: 'blue.500',
+        }}
+        _focus={{
+          outline: 'none',
+          bg: 'white',
+          border: '1px solid',
+          borderColor: 'blue.500',
+        }}
+        bg='gray.50'
+      />
+      <Input
+        required
+        name='confirmPassword'
+        placeholder='confirm password'
+        type='password'
+        mb={2}
+        onChange={onChange}
+        fontSize='10px'
+        _placeholder={{ color: 'gray.500' }}
+        _hover={{
+          bg: 'white',
+          border: '1px solid',
+          borderColor: 'blue.500',
+        }}
+        _focus={{
+          outline: 'none',
+          bg: 'white',
+          border: '1px solid',
+          borderColor: 'blue.500',
+        }}
+        bg='gray.50'
+      />
+      <Button width='100%' height='36px' mb={2} type='submit'>
+        Sign Up
+      </Button>
+      <Flex fontSize='9pt' justifyContent='center'>
+        <Text mr={1}>Already a redditor?</Text>
+        <Text
+          color='blue.500'
+          fontWeight={700}
+          cursor='pointer'
+          onClick={() =>
+            setAuthModalState((prev) => ({
+              ...prev,
+              view: 'login',
+            }))
+          }
+        >
+          LOG IN
+        </Text>
+      </Flex>
+    </form>
+  )
+}
+export default SignUp
