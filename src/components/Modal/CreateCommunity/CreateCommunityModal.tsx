3720970dcd59d37e1b2e05153d633b2694acdbc3
osapiyo <osapiyo1@gmail.com>
2022-06-04 11:36:56 +0900

    added community modal

--- src/components/Modal/CreateCommunity/CreateCommunityModal.tsx
+++ src/components/Modal/CreateCommunity/CreateCommunityModal.tsx
@@ -7,8 +7,14 @@ import {
   ModalCloseButton,
   ModalBody,
   ModalFooter,
+  Box,
+  Divider,
+  Text,
+  Input,
+  Stack,
+  Checkbox,
 } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react'
 
 type CreateCommunityModalProps = {
   open: boolean
@@ -19,14 +25,84 @@ const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({
   open,
   handleClose,
 }) => {
+  const [communityName, setCommunityName] = useState('')
+  const [charsRemaining, setCharsRemaining] = useState(21)
+  const [communityType, setCommunityType] = useState('public')
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value.length > 21) return
+
+    setCommunityName(event.target.value)
+    setCharsRemaining(21 - event.target.value.length)
+  }
+
+  const onCommunityTypeChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setCommunityType(event.target.name)
+  }
   return (
     <>
       <Modal isOpen={open} onClose={handleClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>here is the modal boby</ModalBody>
+          <ModalHeader
+            display='flex'
+            flexDirection='column'
+            fontSize={15}
+            padding={3}
+          >
+            Create a community
+          </ModalHeader>
+          <Box pl={3} pr={3}>
+            <Divider />
+            <ModalCloseButton />
+            <ModalBody display='flex' flexDirection='column' padding='10px 0px'>
+              <Text fontWeight={600} fontSize={15}>
+                Name
+              </Text>
+              <Text fontSize={11} color='gray.500'>
+                Community names including capitalization cannot be changed
+              </Text>
+              <Text
+                position='relative'
+                top='28px'
+                left='10px'
+                width='20px'
+                color='gray.400'
+              >
+                r/
+              </Text>
+              <Input
+                position='relative'
+                value={communityName}
+                size='sm'
+                pl='22px'
+                onChange={handleChange}
+              />
+              <Text
+                fontSize='11px'
+                color={charsRemaining === 0 ? 'red' : 'gray.500'}
+              >
+                {charsRemaining} Characters remaininsg
+              </Text>
+              <Box mt={4} mb={4}>
+                <Text fontWeight={600} fontSize={15}>
+                  Community Type
+                </Text>
+                <Stack spacing={2}>
+                  <Checkbox
+                    name='public'
+                    isChecked={communityType === 'public'}
+                  >
+                    Public
+                  </Checkbox>
+                  <Checkbox name='restricted'>Restricted</Checkbox>
+                  <Checkbox name='private'>Private</Checkbox>
+                </Stack>
+              </Box>
+            </ModalBody>
+          </Box>
 
           <ModalFooter>
             <Button colorScheme='blue' mr={3} onClick={handleClose}>
