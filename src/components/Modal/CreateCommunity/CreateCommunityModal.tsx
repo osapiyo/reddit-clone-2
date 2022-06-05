25715ad8c8ff3ec6560fbac937ad301c44e1bfb1
osapiyo <osapiyo1@gmail.com>
2022-06-05 23:25:34 +0900

    fixed create community modal

--- src/components/Modal/CreateCommunity/CreateCommunityModal.tsx
+++ src/components/Modal/CreateCommunity/CreateCommunityModal.tsx
@@ -13,8 +13,12 @@ import {
   Input,
   Stack,
   Checkbox,
+  Icon,
+  Flex,
 } from '@chakra-ui/react'
 import React, { useState } from 'react'
+import { BsFillEyeFill, BsFillPersonFill } from 'react-icons/bs'
+import { HiLockClosed } from 'react-icons/hi'
 
 type CreateCommunityModalProps = {
   open: boolean
@@ -43,7 +47,7 @@ const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({
   }
   return (
     <>
-      <Modal isOpen={open} onClose={handleClose}>
+      <Modal isOpen={open} onClose={handleClose} size='lg'>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader
@@ -94,21 +98,65 @@ const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({
                   <Checkbox
                     name='public'
                     isChecked={communityType === 'public'}
+                    onChange={onCommunityTypeChange}
                   >
-                    Public
+                    <Flex align='center'>
+                      <Icon as={BsFillPersonFill} color='gray.500' mr={2} />
+                      <Text fontSize='14px' mr={1}>
+                        Public
+                      </Text>
+                      <Text fontSize='11px' color='gray.500' pt={1}>
+                        Anyone can view, post, and comment to this community
+                      </Text>
+                    </Flex>
                   </Checkbox>
-                  <Checkbox name='restricted'>Restricted</Checkbox>
-                  <Checkbox name='private'>Private</Checkbox>
+                  <Checkbox
+                    name='restricted'
+                    isChecked={communityType === 'restricted'}
+                    onChange={onCommunityTypeChange}
+                  >
+                    <Flex align='center'>
+                      <Icon as={BsFillEyeFill} color='gray.500' mr={2} />
+                      <Text fontSize='14px' mr={1}>
+                        Restricted
+                      </Text>
+                      <Text fontSize='11px' color='gray.500' pt={1}>
+                        Anyone can view this community, but only approved users
+                        can post
+                      </Text>
+                    </Flex>
+                  </Checkbox>
+                  <Checkbox
+                    name='private'
+                    isChecked={communityType === 'private'}
+                    onChange={onCommunityTypeChange}
+                  >
+                    <Flex align='center'>
+                      <Icon as={HiLockClosed} color='gray.500' mr={2} />
+                      <Text fontSize='14px' mr={1}>
+                        Private
+                      </Text>
+                      <Text fontSize='11px' color='gray.500' pt={1}>
+                        only approved users can view and submit to this
+                        community
+                      </Text>
+                    </Flex>
+                  </Checkbox>
                 </Stack>
               </Box>
             </ModalBody>
           </Box>
 
-          <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={handleClose}>
-              Close
+          <ModalFooter bg='gray.100' borderRadius='0px 0px 10px 10px'>
+            <Button
+              variant='outline'
+              height='30px'
+              mr={3}
+              onClick={handleClose}
+            >
+              Cancel
             </Button>
-            <Button variant='ghost'>Create Community</Button>
+            <Button height='30px'>Create Community</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
