13946f205541d7955ae9318e8acb9ce0a13577a1
osapiyo <osapiyo1@gmail.com>
2022-06-17 00:54:23 +0900

    created right hand side

--- src/atoms/communitiesAtom.ts
+++ src/atoms/communitiesAtom.ts
@@ -17,6 +17,7 @@ export interface CommunitySnippet {
 
 interface CommunityState {
   mySnippets: CommunitySnippet[]
+  currentCommnity?: Community
 }
 
 const defaultCommunityState: CommunityState = {
--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -1,7 +1,7 @@
 import { doc, getDoc } from 'firebase/firestore'
 import { GetServerSidePropsContext } from 'next'
-import React from 'react'
-import { Community } from '../../../atoms/communitiesAtom'
+import React, { useEffect } from 'react'
+import { Community, communityState } from '../../../atoms/communitiesAtom'
 import { firestore } from '../../../firebase/clientApp'
 import safeJsonStringify from 'safe-json-stringify'
 import NotFound from '../../../components/Community/NotFound'
@@ -9,15 +9,27 @@ import Header from '../../../components/Community/Header'
 import PageContent from '../../../components/Layout/PageContent'
 import CreatePostLink from '../../../components/Community/CreatePostLink'
 import Posts from '../../../components/Posts/Posts'
+import About from '../../../components/Community/About'
+import { useSetRecoilState } from 'recoil'
 
 type CommunityPageProps = {
   communityData: Community
 }
 
 const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
+  const setCommunityStateValue = useSetRecoilState(communityState)
+
   if (!communityData) {
     return <NotFound />
   }
+
+  useEffect(() => {
+    setCommunityStateValue((prev) => ({
+      ...prev,
+      currentCommnity: communityData,
+    }))
+  }, [])
+
   return (
     <>
       <Header communityData={communityData} />
@@ -27,7 +39,7 @@ const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
           <Posts communityData={communityData} />
         </>
         <>
-          <div>RHS</div>
+          <About communityData={communityData} />
         </>
       </PageContent>
     </>
